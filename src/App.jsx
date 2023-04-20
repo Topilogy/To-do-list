957a854de3f0cfd49b061fd14fc26b16ec5b042e
Topilogy <36141664+Topilogy@users.noreply.github.com>
2023-04-20 20:48:25 +0100

    hey

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,82 @@
-import React, { useState, useEffect } from "react";
-import Header from "./components/Header";
-import Form from "./components/Form";
-import TodoList from "./components/TodoList";
+import React, { useState } from 'react';
 import "./App.css";
-import { stringify } from "uuid";
+import TodoInput from "./components/TodoInput"
+import TodoItem from "./components/TodoItem"
+import Header from "./components/Header";
 
-const App = () => {
+function App() {
+  const [item, setItem]= useState({})
+  const [todoItems, setTodoItems] = useState(
+    [
+    {todo: 'Title:', complete: false, id: 1},
+    {todo: 'Description:', complete: false, id: 2},
+    {todo: 'Due_date:', complete: false, id: 3},
+    {todo: 'Status:', complete: false, id: 4}
+    ]
+    
+    
+    )
 
-  const initialState = JSON.parse(localStorage.getItem("todos")) || [];
-  const [input, setInput] = useState("");
-  const [todos, setTodos] = useState(initialState);
-  const [editTodo, setEditTodos] = useState(null);
+    // Complete Item
+    const completeTodoItem = (index) => {
+      const newTodoItems = [...todoItems];
+      newTodoItems[index].complete = !newTodoItems[index].complete;
+      setTodoItems(newTodoItems)
+      };
 
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos))
-  }, [todos]);
-  return (
-    <div className="container">
-      <div className="app-wrapper">
-        <div>
-          <Header />
-        </div>
+    // Update Item
+    const updateTodoItem = (index) => {
+      const newTodoItems = [...todoItems];
+      const item = newTodoItems[index];
+      setItem(item);
+      // const newItem = prompt(`Update ${item.todo}?`, item.todo);
 
-        <div>
-          <Form 
-            input={input}
-            setInput={setInput}
-            todos={todos}
-            setTodos={setTodos}
-            editTodo={editTodo}
-            setEditTodos={setEditTodos}
-          />
-        </div>
 
-        <div>
-          <TodoList 
-            todos={todos} 
-            setTodos={setTodos}
-            setEditTodos={setEditTodos}  
-          />
-        </div>
+
+      // let todoObj = { todo: newItem, complete: false };
+      // newTodoItems.splice(index, 1, todoObj);
+      // if (newItem === null || newItem === "") {
+      // return;
+      // } else {
+      // item.todo = newItem;
+      // }
+      // setTodoItems(newTodoItems);
+      };
+
+    // Delete Item
+    const deleteTodoItem = (index) => {
+      const newTodoItems = [...todoItems]
+      newTodoItems.splice(index, 1)
+      setTodoItems(newTodoItems)
+    }
+    
+      // Create Item
+    const createTodoItem = (todo) => {
+      const newTodoItems = [...todoItems, { todo, complete: false }];
+      setTodoItems(newTodoItems);
+    };
+
+    const editTodoItem = (todo) => {
+      const foundIndex = todoItems?.findIndex(todoItem => todoItem?.id === todo?.id);
+      todoItems[foundIndex] = todo;
+      setTodoItems([...todoItems]);
+
+
+      setItem({});
+    };
+
+
+    return <div className="container">
+      <div className='app-wrapper'>
+        <div> <Header /> </div>
+        <TodoInput createTodoItem={createTodoItem} editTodoItem={editTodoItem} item={item} />
+          {todoItems?.map((item, index) => (
+        <TodoItem key={index} index={index} item={item} 
+        deleteTodoItem = {deleteTodoItem} 
+        completeTodoItem = {completeTodoItem} 
+        updateTodoItem = {updateTodoItem} />    
+        ))}
       </div>
-    </div>
-  )
-};
-
+    </div>;
+}
 export default App;
---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react'
+
+const TodoInput = ({createTodoItem, editTodoItem, item}) => {
+const [value, setValue] = useState("")
+
+const handleSubmit = e => {
+     e.preventDefault();
+     if(!value) return;
+//     if(value === "") {
+//         return console.log("Please add something to-do")
+//     }
+    if(item?.todo){
+        const updatedTodo = { ...item, todo: value };
+        console.log('updatedTodo', updatedTodo)
+        editTodoItem(updatedTodo);       
+    } else {
+        console.log('createeeee')
+        createTodoItem(value)
+    }
+
+    // createTodoItem(value)
+    setValue("")
+}
+
+useEffect(() => {
+   setValue(item.todo)
+  }, [item]);
+
+    return (
+        <form>
+        <input
+        type="text"
+        className="task-input"
+        placeholder={item === 2 ? 'My placeholder' : 'Your placeholder'}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        />
+        <button className="button-add" onClick={handleSubmit}>{item?.todo ? "UPDATE" : "ADD"}</button>
+
+        </form>
+        )
+}
+export default TodoInput
